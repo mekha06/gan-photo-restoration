b9fca2dbe5b4c1b6f5995a9a8e623518ff459430
Mekha sr <megiie06@gmail.com>
2026-07-22 14:53:36 +0530

    added config.py for the model for easy reuseability

---
+++ config.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+# -----------------------
+# Project Paths
+# -----------------------
+
+ROOT_DIR = Path(__file__).resolve().parent
+
+DATASET_DIR = ROOT_DIR / "dataset"
+
+CLEAN_DIR = DATASET_DIR / "clean"
+
+DAMAGED_DIR = DATASET_DIR / "damaged"
+
+OUTPUT_DIR = ROOT_DIR / "outputs"
+
+CHECKPOINT_DIR = OUTPUT_DIR / "checkpoints"
+
+RESULTS_DIR = OUTPUT_DIR / "results"
+
+# -----------------------
+# Image Parameters
+# -----------------------
+
+IMAGE_SIZE = 128
+
+CHANNELS = 3
+
+# -----------------------
+# Training Parameters
+# -----------------------
+
+BATCH_SIZE = 1
+
+NUM_EPOCHS = 100
+
+LEARNING_RATE = 2e-4
+
+BETA1 = 0.5
+
+BETA2 = 0.999
+
+NUM_WORKERS = 0
+
+# -----------------------
+# Device
+# -----------------------
+
+DEVICE = "cpu"
