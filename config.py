9f0074654f2fbcac6a3836357e5fe41a20e20b21
Mekha sr <megiie06@gmail.com>
2026-07-24 12:57:29 +0530

    added the path of kaggle dataset

--- config.py
+++ config.py
@@ -1,49 +1,29 @@
 from pathlib import Path
 import torch
-# -----------------------
-# Project Paths
-# -----------------------
 
 ROOT_DIR = Path(__file__).resolve().parent
 
 DATASET_DIR = ROOT_DIR / "dataset"
 
-CLEAN_DIR = DATASET_DIR / "clean"
+CLEAN_DIR = Path("/kaggle/input/datasets/denislukovnikov/ffhq256-images-only/ffhq256")
 
 DAMAGED_DIR = DATASET_DIR / "damaged"
 
 OUTPUT_DIR = ROOT_DIR / "outputs"
-
 CHECKPOINT_DIR = OUTPUT_DIR / "checkpoints"
-
+SAMPLES_DIR = OUTPUT_DIR / "samples"
 RESULTS_DIR = OUTPUT_DIR / "results"
 
-# -----------------------
-# Image Parameters
-# -----------------------
-
 IMAGE_SIZE = 256
-
 CHANNELS = 3
 
-# -----------------------
-# Training Parameters
-# -----------------------
-
-BATCH_SIZE = 1
-
+BATCH_SIZE = 8
 NUM_EPOCHS = 100
 
 LEARNING_RATE = 2e-4
-
 BETA1 = 0.5
-
 BETA2 = 0.999
 
-NUM_WORKERS = 0
-
-# -----------------------
-# Device
-# -----------------------
+NUM_WORKERS = 2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
